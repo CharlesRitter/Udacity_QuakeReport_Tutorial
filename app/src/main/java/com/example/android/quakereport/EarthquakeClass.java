db0975e813f6596ca3e3e005ccbf3401abe19d59
Charles Ritter <crritte@g.clemson.edu>
2018-07-02 12:52:24 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/quakereport/EarthquakeClass.java
@@ -0,0 +1,53 @@
+package com.example.android.quakereport;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class EarthquakeClass {
+    private String loc;
+    private Double mag;
+    private Long unixTime;
+    private String url;
+
+    EarthquakeClass(String l, Double m, Long t, String u){
+        loc = l;
+        mag = m;
+        unixTime = t;
+        url = u;
+    }
+
+    public Double getMagnitude(){
+        return mag;
+    }
+
+    public String getLoc(){
+        return(loc);
+    }
+
+    public String getUrl(){
+        return(url);
+    }
+
+
+    public String getDate(){
+        Date unixDate = new Date(unixTime);
+        SimpleDateFormat fmtStr = new SimpleDateFormat("MMM DD, yyyy");
+        String date = fmtStr.format(unixDate);
+
+        return date;
+    }
+
+    public String getTime(){
+        Date milliTime = new Date(unixTime);
+        SimpleDateFormat fmtStr = new SimpleDateFormat("hh:mm aa");
+        String time = fmtStr.format(milliTime);
+
+        return time;
+    }
+
+    @Override
+    public String toString(){
+        return(String.format(Locale.US, "%-3.1f %s %s",mag, loc, getDate()));
+    }
+}
---
+++ app/src/main/java/com/example/android/quakereport/EqAdapter.java
@@ -0,0 +1,99 @@
+package com.example.android.quakereport;
+
+import android.app.Activity;
+import android.graphics.drawable.GradientDrawable;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class EqAdapter extends ArrayAdapter<EarthquakeClass> {
+
+    public EqAdapter(Activity context, ArrayList<EarthquakeClass> eqList){
+        super(context, 0, eqList);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.custom_list, parent, false);
+        }
+
+        EarthquakeClass currentItem = getItem(position);
+
+        //Print the magnitude of the quake
+        TextView magnitudeView = (TextView) listItemView.findViewById(R.id.magnitude);
+        DecimalFormat dblStr = new DecimalFormat("0.0");
+        magnitudeView.setText(dblStr.format(currentItem.getMagnitude()));
+        //set the background color for the magnitude
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeView.getBackground();
+        int magnitudeColor = getMagnitudeColor(currentItem.getMagnitude());
+        magnitudeCircle.setColor(magnitudeColor);
+
+        //Print the location of the quake
+        TextView offsetView= (TextView) listItemView.findViewById(R.id.offset);
+        TextView locView = (TextView) listItemView.findViewById(R.id.location);
+
+        String unformattedLoc = currentItem.getLoc();
+        //there are two different formats for the "name" string and so there are two
+        //possibilities for how it will be printed
+        if(unformattedLoc.contains("of")){
+            int split = unformattedLoc.indexOf("of") + 2;
+            String offset = unformattedLoc.substring(0, split);
+            String loc = unformattedLoc.substring(split + 1);
+
+            offsetView.setText(offset);
+            locView.setText(loc);
+        } else {
+            offsetView.setText("Near the");
+            locView.setText(unformattedLoc);
+        }
+
+        //print the date of the quake
+        TextView dateView = (TextView) listItemView.findViewById(R.id.date);
+        dateView.setText(currentItem.getDate());
+
+        //print the time of the quake
+        TextView timeView = (TextView) listItemView.findViewById(R.id.time);
+        timeView.setText(currentItem.getTime());
+
+        return(listItemView);
+    }
+
+    private int getMagnitudeColor(Double magnitude){
+        switch (magnitude.intValue())
+        {
+            case 0:
+            case 1:
+                return(ContextCompat.getColor(getContext(), R.color.magnitude1));
+            case 2:
+                return(ContextCompat.getColor(getContext(), R.color.magnitude2));
+            case 3:
+                return(ContextCompat.getColor(getContext(), R.color.magnitude3));
+            case 4:
+                return(ContextCompat.getColor(getContext(), R.color.magnitude4));
+            case 5:
+                return(ContextCompat.getColor(getContext(), R.color.magnitude5));
+            case 6:
+                return(ContextCompat.getColor(getContext(), R.color.magnitude6));
+            case 7:
+                return(ContextCompat.getColor(getContext(), R.color.magnitude7));
+            case 8:
+                return(ContextCompat.getColor(getContext(), R.color.magnitude8));
+            case 9:
+                return(ContextCompat.getColor(getContext(), R.color.magnitude9));
+            default:
+                return(ContextCompat.getColor(getContext(), R.color.magnitude10plus));
+        }
+    }
+}
